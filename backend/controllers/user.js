aa0cf152b19ef92262a09fa03ae317c8f424c10a
sanaa <sanaaelba@hotmail.fr>
2022-01-20 23:03:35 +0100

    Ajustement

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -3,23 +3,24 @@
 const User = require('../models/User');
 
 //Importer bcrypt
-//const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 //Importer jwt
 //const jwt = require('jsonwebtoken');
 
 //Fonction signup pour enregistrer des nouveaux utilisateurs
-exports.signup = (req, res, ) => {
-    //bcrypt.hash(req.body.password, 10)
-     // .then(hash => {
-        const user = new User({
-          email: req.body.email,
-          password: req.body.password
-        });
-       // user.save()
-         // .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-          //.catch(error => res.status(400).json({ error }));
-      };
-      //.catch(error => res.status(500).json({ error }));
-      console.log(user);
-    
+exports.signup = (req, res, next) => {
+  bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+      const user = new User({
+        email: req.body.email,
+        password: hash
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+        .catch(error => res.status(400).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+
