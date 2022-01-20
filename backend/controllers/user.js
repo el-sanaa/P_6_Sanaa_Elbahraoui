60f1e5499bbcd59c7da6a0cf68f9595d29c89e11
sanaa <sanaaelba@hotmail.fr>
2022-01-20 19:47:12 +0100

    correction

---
+++ backend/controllers/user.js
@@ -0,0 +1,25 @@
+//Controller de la route
+//Importer models de la base de donnée User.js
+const User = require('../models/User');
+
+//Importer bcrypt
+//const bcrypt = require('bcrypt');
+
+//Importer jwt
+//const jwt = require('jsonwebtoken');
+
+//Fonction signup pour enregistrer des nouveaux utilisateurs
+exports.signup = (req, res, ) => {
+    //bcrypt.hash(req.body.password, 10)
+     // .then(hash => {
+        const user = new User({
+          email: req.body.email,
+          password: req.body.password
+        });
+       // user.save()
+         // .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
+          //.catch(error => res.status(400).json({ error }));
+      };
+      //.catch(error => res.status(500).json({ error }));
+      console.log(user);
+    
---
+++ backend/models/Users.js
@@ -0,0 +1,16 @@
+//Partie identification
+//Importer mongoose
+const mongoose = require('mongoose');
+
+//Créer schema de données pour la base de données MongoDB.
+//Définir la forme du documents
+const userSchema = mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+   
+});
+
+
+
+//Exporter mongoose.model
+module.exports = mongoose.Model("user", userSchema);
